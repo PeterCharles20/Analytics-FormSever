1c4612cbb69cf49d2e63fb91cb58ab887b4926b6
Brian <bv457@uowmail.edu.au>
2019-10-31 11:39:01 +1100

    Final tocuh

--- index.js
+++ index.js
@@ -7,8 +7,25 @@
   * Define modules
   */
 const express = require('express');
-const obj = require("../Analytics-FormSever/responses.json");
+// const obj = require("../Analytics-FormSever/responses.json");
 const cors = require('cors');
+const http = require('http');
+var request=require('request');
+
+const options = {
+	hostname: 'mysite.com',
+	port: 80,
+	path: '/get-analytics',
+	method: 'GET',
+	headers: {
+    	'Content-Type': 'application/json'
+  	}
+}
+
+// const req = http.request(options);
+// req.end();
+
+
 
 let app = express();
 
@@ -25,13 +42,40 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+var obj;
 /**
  * Returns a array of mock survey responses
  */
+ var url = 'http://mysite.com/get-analytics';
 app.get('/responses', (req, res) => {
-    res.json(obj);
-});
+	
+	http.get(url, function(res){
+    var body = '';
 
+	    res.on('data', function(chunk){
+	        body += chunk;
+	    });
+
+	    res.on('end', function(){
+	        var fbResponse = JSON.parse(body);
+	        if(body === null) {
+	        	console.log("n7ulls");
+	        } else {
+	        	// console.log(body);
+	        	obj = body;
+	        }
+	    });
+		}).on('error', function(e){
+	      console.log("Got an error: ", e);
+		});
+ 	console.log(obj);
+ 	if(obj !== null) {
+ 		res.send(obj);
+ 	} else {
+ 		res.send(null);
+ 	}
+  	
+});
 
 /**
  * Listen
@@ -41,3 +85,4 @@ app.listen(port, function()
 {
 	console.log('Example app listening on port ' + port + "!");
 });
+
